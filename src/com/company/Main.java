e4b265f2a558bbec86f10cd48528ac98c085480b
Tony <tony.ratley430@gmail.com>
2019-02-07 23:02:05 -0600

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Main
+    {
+        
+        public static void main(String[] args)
+            {
+                sayHelloWorld();
+            }
+        
+        private static void sayHelloWorld()
+            {
+                System.out.println("Hello, world.");
+                sayHelloWorld();
+            }
+    }
